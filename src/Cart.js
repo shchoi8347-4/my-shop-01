ce657fd7db4cffefe975b28a692b6e4d6c98f3ee
shchoi8347-2 <shchoi8347@gmail.com>
2022-01-27 15:52:19 +0900

    commit to Cart.js

---
+++ src/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Cart.css';
+
+class Cart extends React.Component {
+    render() {
+        let output = <div><h2>Cart</h2></div>;
+        return output;
+    }
+}
+
+export default Cart;
